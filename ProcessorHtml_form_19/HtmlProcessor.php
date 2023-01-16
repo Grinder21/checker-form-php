51c27bc965c31ae59e5908bc7743cc55fae54814
Grinder21 <spartawinner280@mail.ru>
2023-01-17 00:29:13 +0300

    realization new func

--- ProcessorHtml_form_19/HtmlProcessor.php
+++ ProcessorHtml_form_19/HtmlProcessor.php
@@ -2,10 +2,14 @@
 
 
 if ($_SERVER["REQUEST_METHOD"]) {
-        $name = json_decode($_POST['json'], 1);
-        $pattern = '/\<a\s.*?\>(.*?)\<\/a\>/iums';
-        echo preg_replace($pattern, '', $name);
-        header('Content-Type: application/json');
+        if (json_decode($_POST['json']) == '') {
+            http_response_code(500);
+        } else {
+            $name = json_decode($_POST['json'], 1);
+            $pattern = '/\<a\s.*?\>(.*?)\<\/a\>/iums';
+            echo preg_replace($pattern, '', $name);
+            header('Content-Type: application/json');
+        }
 }
 
 
